4d87e2371595965d3d3bdca8a489b8bc172b5c89
EstebArdo <103789720+EstebArdo@users.noreply.github.com>
2025-03-08 16:13:10 -0600

    Add files via upload

--- Objects and Values, Identical or Equivalent.py
+++ Objects and Values, Identical or Equivalent.py
@@ -13,3 +13,20 @@ c is d
 #   that are equivalent.
 # When you create two lists, you get two objects
 
+#############################################
+# Aliasing
+
+e = 'apple'
+f = e
+e is f
+# True - f is an alias to e.  both refer to the same object.
+# e and f are variables that reference the same object.
+
+# object with  more than 1 reference has more than one name.
+# So we say this object is "Aliased."
+###########################################
+# if the object is mutable.
+# changes to one alias affect the other.
+
+#usually avoid aliasing to avoid errors with mmutable objects.
+# For MImmutable objects like strings, it is not much of a problem.
